27d1a41e1ccf2d5759f36e32d6ec692d9247a937
adfolk <adfolkestad@gmail.com>
2025-03-12 21:20:31 +0400

    Completed and tested Cell class

--- src/geometry.py
+++ src/geometry.py
@@ -1,4 +1,3 @@
-# note- this will contain the Point, Line, and Cell classes.
 from tkinter import Canvas
 
 class Point:
@@ -8,9 +7,46 @@ class Point:
 
 class Line:
     def __init__(self, point_a: Point, point_b: Point):
-        self.__a = point_a
-        self.__b = point_b
+        self._a = point_a
+        self._b = point_b
 
     def draw(self, canvas: Canvas, fill_color: str):
-        canvas.create_line(self.__a.x, self.__a.y, self.__b.x, self.__b.y, fill=fill_color, width=2)
+        canvas.create_line(self._a.x, self._a.y, self._b.x, self._b.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, x1: int, x2: int, y1: int, y2: int, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+
+        self._top_left = Point(self._x1, self._y1)
+        self._top_right = Point(self._x2, self._y1)
+        self._bottom_left = Point(self._x1, self._y2)
+        self._bottom_right = Point(self._x2, self._y2)
+
+        self._left_wall = Line(self._top_left, self._bottom_left)
+        self._right_wall = Line(self._top_right, self._bottom_right)
+        self._top_wall = Line(self._top_left, self._top_right)
+        self._bottom_wall = Line(self._bottom_left, self._bottom_right)
+
+        self._win = win
+    
+    def draw(self):
+        if self.has_left_wall:
+            self._win.draw_line(self._left_wall, fill_color="black")
+
+        if self.has_right_wall:
+            self._win.draw_line(self._right_wall, fill_color="black")
+
+        if self.has_top_wall:
+            self._win.draw_line(self._top_wall, fill_color="black")
+
+        if self.has_bottom_wall:
+            self._win.draw_line(self._bottom_wall, fill_color="black")
 
--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
-from window import Window
-from geometry import Point, Line
+from screen import Window
+from geometry import Point, Line, Cell
 
 def main():
     win = Window(800, 600)
-    pt_a = Point(69, 420)
-    pt_b = Point(420, 69)
-    test_line = Line(pt_a, pt_b)
-    win.draw_line(test_line, "black")
+    #pt_a = Point(69, 420)
+    #pt_b = Point(420, 69)
+    #test_line = Line(pt_a, pt_b)
+    #win.draw_line(test_line, "black")
+
+    test_cell = Cell(x1=150, x2=200, y1=150, y2=200, win=win)
+    test_cell.has_left_wall = False
+    test_cell.has_right_wall = False
+    test_cell.has_top_wall = False
+    test_cell.has_bottom_wall = False
+    test_cell.draw()
+
     win.wait_for_close()
 
 main()
